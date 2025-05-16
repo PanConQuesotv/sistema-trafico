b401e8ecad5eb046f887256ebb5edd078c797a72
Juan Sebastian Garzon Gomez <sebastiangarzongomez12@gmail.com>
2025-05-15 22:29:02 -0500

    Pages CSS6?

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -1,18 +1,14 @@
-import '../App.css';
-
 function Inicio() {
   return (
-    <div className="app">
-      <div className="overlay">
-        <h1>Sistema de Tráfico Urbano</h1>
-        <p>
-          Consulta datos reales de tráfico y analiza el comportamiento vehicular en tiempo real.
-        </p>
-        <button onClick={() => alert("Próximamente: panel de visualización")}>
-          Ver panel de datos
-        </button>
-      </div>
-    </div>
+    <>
+      <h1>Sistema de Tráfico Urbano</h1>
+      <p>
+        Consulta datos reales de tráfico y analiza el comportamiento vehicular en tiempo real.
+      </p>
+      <button onClick={() => alert("Próximamente: panel de visualización")}>
+        Ver panel de datos
+      </button>
+    </>
   );
 }
 
