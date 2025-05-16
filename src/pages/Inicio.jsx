e274a81aae92a6950a6980d073c51ed7a2e6fa45
Juan Sebastian Garzon Gomez <sebastiangarzongomez12@gmail.com>
2025-05-15 23:21:21 -0500

    2B vs Eve escenario mi cama

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -1,9 +1,11 @@
 function Inicio() {
   return (
-    <div>
-      <h1>Bienvenido</h1>
-      <p>Este es el inicio del sistema de tráfico.</p>
-      <button className="cta-button">Comenzar</button>
+    <div className="app">
+      <div className="overlay">
+        <h1>Bienvenido</h1>
+        <p>Este es el inicio del sistema de tráfico.</p>
+        <button className="cta-button">Comenzar</button>
+      </div>
     </div>
   );
 }
