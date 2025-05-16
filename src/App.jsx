215420b12b79b8c93f553acf201739cabdd99f5f
Juan Sebastian Garzon Gomez <sebastiangarzongomez12@gmail.com>
2025-05-15 22:43:06 -0500

    Pages Prueba3

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,27 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import './App.css';
 import Navbar from './components/Navbar';
-import Inicio from './pages/Inicio';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Dashboard from './pages/Dashboard';
 import Arquitectura from './pages/Arquitectura';
+import Inicio from './pages/Inicio';
 import Simulacion from './pages/Simulacion';
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Inicio />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/arquitectura" element={<Arquitectura />} />
-        <Route path="/simulacion" element={<Simulacion />} />
-      </Routes>
-    </Router>
+    <div className="app">
+      <div className="overlay">
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Inicio />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/arquitectura" element={<Arquitectura />} />
+            <Route path="/simulacion" element={<Simulacion />} />
+          </Routes>
+        </Router>
+      </div>
+    </div>
   );
 }
 
