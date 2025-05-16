8f9fbaefa7e82b3a351603438be2550d9641a832
Juan Sebastian Garzon Gomez <sebastiangarzongomez12@gmail.com>
2025-05-15 23:45:14 -0500

    Quiero Stellar Blade en sangre

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -4,7 +4,11 @@ function Inicio() {
       <div className="overlay">
         <h1>Bienvenido</h1>
         <p>Este es el inicio del sistema de tráfico.</p>
-        <button className="cta-button">Comenzar</button>
+        <button
+          className="cta-button"
+          onClick={() => alert('Próximamente.')}
+        ></button>
+        
       </div>
     </div>
   );
