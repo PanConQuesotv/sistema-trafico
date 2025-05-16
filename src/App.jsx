1c092df23bc87e69e66cc25a2e8803a74a657f27
Juan Sebastian Garzon Gomez <sebastiangarzongomez12@gmail.com>
2025-05-15 19:42:01 -0500

    Help no puedo jugar stellar en mi pc

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
+
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+    <div className="app-container">
+      <div className="overlay">
+        <h1>Sistema de Monitoreo de Tráfico Urbano</h1>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Visualiza, analiza y toma decisiones inteligentes con datos de tráfico en tiempo real.
         </p>
+        <button onClick={() => alert("Próximamente: panel de visualización")}>
+          Ver panel de datos
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
