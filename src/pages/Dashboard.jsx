709ab8fa15212c9a2345faa1bfce7ef5b18c7e67
Juan Sebastian Garzon Gomez <sebastiangarzongomez12@gmail.com>
2025-05-15 20:49:36 -0500

    Pages CSS2?

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,9 +2,11 @@ import '../App.css';
 
 function Dashboard() {
   return (
-    <div style={{ padding: '2rem', color: '#333' }}>
-      <h1>Tablero de Control</h1>
-      <p>Aquí se visualizarán los datos del tráfico urbano en tiempo real.</p>
+    <div className="app">
+      <div className="overlay">
+        <h1>Tablero de Control</h1>
+        <p>Aquí se visualizarán los datos del tráfico urbano en tiempo real.</p>
+      </div>
     </div>
   );
 }
