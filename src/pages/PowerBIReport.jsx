b681eac0318344c14c0e07857f5cbf71809eef47
Juan Sebastian Garzon Gomez <sebastiangarzongomez12@gmail.com>
2025-05-23 23:35:49 -0500

    Prueba pOWERBI6

--- src/pages/PowerBIReport.jsx
+++ src/pages/PowerBIReport.jsx
@@ -1,20 +1,19 @@
-import './PowerBIReport.css';
-
 function PowerBIReport() {
   return (
-    <div className="overlay">
-      <h1>Panel de Power BI</h1>
-      <iframe
-        title="TraficGuard"
-        width="100%"
-        height="700"
-        src="https://app.powerbi.com/view?r=eyJrIjoiZDNhYjc3Y2ItYTRiYi00OGRjLWEwMDItOGYxMzQzOTg3OTllIiwidCI6IjA3ZGE2N2EwLTFmNDMtNGU4Yy05NzdmLTVmODhiNjQ3MGVlNiIsImMiOjR9"
-        frameBorder="0"
-        allowFullScreen
-        style={{ borderRadius: '12px' }}
-      ></iframe>
+    <div className="app"> {/* << Asegura que herede el fondo */}
+      <div className="overlay">
+        <h1>Panel de Power BI</h1>
+        <iframe
+          title="TraficGuard"
+          width="100%"
+          height="700"
+          src="https://app.powerbi.com/view?r=eyJrIjoiZDNhYjc3Y2ItYTRiYi00OGRjLWEwMDItOGYxMzQzOTg3OTllIiwidCI6IjA3ZGE2N2EwLTFmNDMtNGU4Yy05NzdmLTVmODhiNjQ3MGVlNiIsImMiOjR9"
+
+          frameBorder="0"
+          allowFullScreen
+          style={{ borderRadius: '12px' }}
+        ></iframe>
+      </div>
     </div>
   );
 }
-
-export default PowerBIReport;
