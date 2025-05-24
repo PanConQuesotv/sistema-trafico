ed154d1c683166676416e7ba6ecf3b8f3c9b458c
Juan Sebastian Garzon Gomez <sebastiangarzongomez12@gmail.com>
2025-05-23 23:56:27 -0500

    Prueba images04

--- src/pages/Sistema.jsx
+++ src/pages/Sistema.jsx
@@ -5,17 +5,17 @@ const pasos = [
   {
     titulo: '1. Descarga de Datasets',
     texto: 'Se recopilan datos abiertos sobre tráfico vehicular y peajes desde plataformas oficiales como datos.gov.co.',
-    imagen: 'https://www.stellar-blade.com/resources/front/images/post_ms7/22.jpg'
+    imagen: 'https://i.ibb.co/bMpQW8Zz/daset.jpg'
   },
   {
     titulo: '2. Modelo Relacional',
     texto: 'Los datos se organizan en un modelo relacional que permite estructurar la información y normalizarla.',
-    imagen: 'https://www.stellar-blade.com/resources/front/images/post_ms7/22.jpg'
+    imagen: 'https://i.ibb.co/LhYBL03S/r-ELACIONAL.jpg'
   },
   {
     titulo: '3. Modelo Multidimensional',
     texto: 'Se transforma la base relacional en un modelo estrella para análisis más eficientes en herramientas BI.',
-    imagen: 'https://www.stellar-blade.com/resources/front/images/post_ms7/22.jpg'
+    imagen: 'https://i.ibb.co/C4yC2mY/Multi.jpg'
   },
   {
     titulo: '4. Gráficas en Power BI',
@@ -25,12 +25,12 @@ const pasos = [
   {
     titulo: '5. Proceso ETL',
     texto: 'Se diseña un flujo de extracción, transformación y carga para automatizar el procesamiento de datos.',
-    imagen: 'https://www.stellar-blade.com/resources/front/images/post_ms7/22.jpg'
+    imagen: 'https://i.ibb.co/6c38c8zK/Job.jp'
   },
   {
     titulo: '6. Maqueta e Interfaz',
     texto: 'Se diseña una interfaz gráfica e intuitiva que permite al usuario explorar visualmente el sistema.',
-    imagen: 'https://www.stellar-blade.com/resources/front/images/post_ms7/22.jpg'
+    imagen: 'https://i.ibb.co/27Cpmv2N/Maquta.jpg'
   }
 ];
 
