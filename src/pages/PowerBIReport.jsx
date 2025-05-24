47f63b4d380fd5a5f08cb483538a127d6e5f3616
Juan Sebastian Garzon Gomez <sebastiangarzongomez12@gmail.com>
2025-05-23 23:16:45 -0500

    Prueba pOWERBI2

--- src/pages/PowerBIReport.jsx
+++ src/pages/PowerBIReport.jsx
@@ -2,21 +2,14 @@ import React from 'react';
 import './PowerBIReport.css';
 
 const PowerBIReport = () => {
-  return (
-    <div className="powerbi-container">
-      <h1>Dashboard de Análisis - Power BI</h1>
-      <div className="powerbi-frame-wrapper">
-        <iframe
-          title="Reporte Power BI"
-          width="100%"
-          height="100%"
-          src="https://app.powerbi.com/view?r=TU_LINK_PUBLICADO"
-          frameBorder="0"
-          allowFullScreen
-        ></iframe>
-      </div>
-    </div>
-  );
+return (
+<div className="powerbi-container">
+<h1 className="powerbi-title">Reporte Power BI</h1>
+<div className="powerbi-iframe-wrapper">
+<iframe title="TraficGuard" width="1000" height="700" src="https://app.powerbi.com/view?r=eyJrIjoiZDNhYjc3Y2ItYTRiYi00OGRjLWEwMDItOGYxMzQzOTg3OTllIiwidCI6IjA3ZGE2N2EwLTFmNDMtNGU4Yy05NzdmLTVmODhiNjQ3MGVlNiIsImMiOjR9" frameBorder="0" allowFullScreen={true} ></iframe>
+</div>
+</div>
+);
 };
 
 export default PowerBIReport;
